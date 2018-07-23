e1b5a43ca1c1a62658450c8891790a1a5632cfa7
evgzakharov <evgzakharov88@gmail.com>
2018-07-23 20:17:34 +0300

    remove unnecessary data

--- src/main/java/test/Main.kt
+++ src/main/java/test/Main.kt
@@ -1,46 +1,9 @@
 package test
 
-class Person {
-    operator fun compareTo(person2: Person): Int {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
-
-    operator fun rangeTo(person2: Person): ClosedRange<Int> {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
-
-    operator fun component1(): Any {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
-
-    operator fun component2(): Any {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
-}
-
-data class Container(
-    val value: String,
-    val value2: Int,
-    val test123: Int
-)
-
 fun main(args: Array<String>) {
-    val person1 = Person()
-    val person2 = Person()
 
-//    person1 < person2
-
-//   person1..person2
-
-    val (name, surname) = person1
-
-    val (value, value2, test123) = method()
 }
 
-fun method(): Container {
-    /**
-     * complex
-     */
+fun method() {
 
-    return Container("" , 5 , 55)
 }
