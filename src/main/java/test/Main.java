eca0d97818d5ee17d4ad14c627d2b2b944c81f14
evgzakharov <evgzakharov88@gmail.com>
2018-07-23 18:32:21 +0300

    minor

--- src/main/java/test/Main.java
+++ src/main/java/test/Main.java
@@ -2,12 +2,6 @@ package test;
 
 public class Main {
     public static void main(String[] args) {
-        User user = User.builder()
-                .name("test-name")
-                .surname("test-surname")
-                .age(25)
-                .build();
-
-        System.out.println(user);
+        System.out.println("test");
     }
 }
