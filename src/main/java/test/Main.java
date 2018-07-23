d8026045f58d2d5606fb829ade99f828b61d5a7b
evgzakharov <evgzakharov88@gmail.com>
2018-07-23 19:01:10 +0300

    init work1

--- src/main/java/test/Main.java
+++ src/main/java/test/Main.java
@@ -1,7 +1,24 @@
 package test;
 
+import java.math.BigDecimal;
+
+import static test.MoneyUtils.prettyPrint;
+
 public class Main {
+    public static final String RUB_CURRENCY = "RUB";
+    public static final String USD_CURRENCY = "USD";
+
     public static void main(String[] args) {
-        System.out.println("test");
+        Money money1Rub = new Money(new BigDecimal(10), RUB_CURRENCY);
+        Money money2Rub = new Money(new BigDecimal(5));
+
+        Money sumRub = money1Rub.add(money2Rub);
+        System.out.println(prettyPrint(sumRub));
+
+        Money money1Usd = new Money(new BigDecimal(25), USD_CURRENCY);
+        Money money2Usd = new Money(new BigDecimal(5), USD_CURRENCY);
+
+        Money minUsd = money1Usd.minus(money2Usd);
+        System.out.println(prettyPrint(minUsd));
     }
 }
