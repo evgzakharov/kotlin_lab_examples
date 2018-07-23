320f1231ab3173510fcaf4533a0325276916961c
evgzakharov <evgzakharov88@gmail.com>
2018-07-23 18:11:52 +0300

    init step2

--- src/main/java/test/Main.java
+++ src/main/java/test/Main.java
@@ -2,12 +2,21 @@ package test;
 
 public class Main {
     public static void main(String[] args) {
-        User user = User.builder()
-                .name("test-name")
-                .surname("test-surname")
-                .age(25)
-                .build();
+        User user = new User(
+                "test-user",
+                "test-surname",
+                "test-patronymic",
+                25
+        );
 
         System.out.println(user);
+
+        User user2 = new User();
+        user2.setName("test-user2");
+        user2.setSurname("test-surname2");
+        user2.setPatronymic("test-patronymic2");
+        user2.setAge(55);
+
+        System.out.println(user2);
     }
 }
--- src/main/java/test/User.java
+++ src/main/java/test/User.java
@@ -1,22 +1,81 @@
 package test;
 
-import lombok.Builder;
-import lombok.Getter;
-import lombok.Setter;
-import lombok.ToString;
-
 import java.time.ZonedDateTime;
 
-@Getter
-@Setter
-@ToString
-@Builder
 public class User {
     private String name;
     private String surname;
     private String patronymic;
     private Integer age;
 
-    @Builder.Default
     private ZonedDateTime created = ZonedDateTime.now();
+
+    public User() {
+    }
+
+    public User(String name, String surname, String patronymic, Integer age) {
+        this.name = name;
+        this.surname = surname;
+        this.patronymic = patronymic;
+        this.age = age;
+    }
+
+    public User(String name, String surname, String patronymic, Integer age, ZonedDateTime created) {
+        this.name = name;
+        this.surname = surname;
+        this.patronymic = patronymic;
+        this.age = age;
+        this.created = created;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getPatronymic() {
+        return patronymic;
+    }
+
+    public void setPatronymic(String patronymic) {
+        this.patronymic = patronymic;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public ZonedDateTime getCreated() {
+        return created;
+    }
+
+    public void setCreated(ZonedDateTime created) {
+        this.created = created;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "name='" + name + '\'' +
+                ", surname='" + surname + '\'' +
+                ", patronymic='" + patronymic + '\'' +
+                ", age=" + age +
+                ", created=" + created +
+                '}';
+    }
 }
