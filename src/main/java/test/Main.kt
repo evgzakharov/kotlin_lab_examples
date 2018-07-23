18d7142b4e1188933245aa50eaf932353af59b44
evgzakharov <evgzakharov88@gmail.com>
2018-07-23 22:23:26 +0300

    init full step 7

--- src/main/java/test/Main.kt
+++ src/main/java/test/Main.kt
@@ -1,5 +1,41 @@
 package test
 
+import java.util.stream.Collectors
+
 fun main(args: Array<String>) {
+    /**
+     * sequece
+     */
+    val sequence = sequenceOf(1, 2, 3)
+
+    val seqResult: Sequence<Int> = sequence.map { it * 2 }
+
+    val collect: List<Int> = seqResult.toList()
+
+    /**
+     * list
+     */
+    val list: List<String> = listOf("1", "2", "3")
+
+    val result: List<String> = list.map { it + it }
+
+    /**
+     * stream
+     */
+
+    val collectResult: MutableList<String> = list.stream()
+        .collect(Collectors.toList())
+
+    /**
+     * iteration
+     */
+
+    val collection = listOf(1, 2, 3)
+
+    val collectionResult: MutableList<Int> = mutableListOf()
+    for (value in collection) {
+        collectionResult += value * 5
+    }
+
 
 }
