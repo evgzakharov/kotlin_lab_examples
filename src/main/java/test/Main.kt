1d499494a314ac55fbc55c22b74091a2a3983631
evgzakharov <evgzakharov88@gmail.com>
2018-07-23 20:05:12 +0300

    init

--- src/main/java/test/Main.kt
+++ src/main/java/test/Main.kt
@@ -1,19 +1,9 @@
 package test
 
-interface Expr
+fun main(args: Array<String>) {
 
-class Num(val value: Int) : Expr
-
-class Sum(val left: Expr, val right: Expr) : Expr
-
-fun eval(expr: Expr): Int = when (expr) {
-    is Num -> expr.value
-    is Sum -> eval(expr.left) + eval(expr.right)
-    else -> throw IllegalArgumentException("Unknown expression")
 }
 
-fun main(args: Array<String>) {
-    val expr = Sum(Num(3), Num(2))
+fun method() {
 
-    println(eval(expr))
 }
