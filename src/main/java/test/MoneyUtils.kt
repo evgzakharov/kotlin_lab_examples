0b6bdec11b3191ffbb65557c7eb3172029adf41a
evgzakharov <evgzakharov88@gmail.com>
2018-07-23 19:28:39 +0300

    minor

--- src/main/java/test/MoneyUtils.kt
+++ src/main/java/test/MoneyUtils.kt
@@ -6,8 +6,8 @@ fun Number.bd(): BigDecimal = BigDecimal(this.toDouble())
 
 fun Number.toMoney(currency: String = RUB_CURRENCY): Money = Money(this.bd(), currency)
 
-fun Money.prettyPrint(): String = when {
-    currency == RUB_CURRENCY -> "$value₽"
-    currency == USD_CURRENCY -> "$value$"
+fun Money.prettyPrint(): String = when (currency) {
+    RUB_CURRENCY -> "$value₽"
+    USD_CURRENCY -> "$value$"
     else -> "$value $currency"
 }
